a8ea1d105dbf74ce6164100bd54aaa00263b7927
Listen <635099067@qq.com>
2019-09-02 11:21:38 +0800

    type: feat: 增加 jsx 语法等笔记 pref: 删除无用代码注释

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+// 定义组件
+// 正常返回应该是字符串类，使用 jsx 语法可直接返回标签
 function App() {
   return (
     <div>
-      Hellow, world!
+      花括号可写 js 表达式<br/>
+      { 1 + 2 }<br/>
+      Hellow, world!<br/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+// 以大写字母开头都是组件
 import React from 'react';
+
+// 把组件挂载到 dom 节点上
 import ReactDOM from 'react-dom';
 import App from './App';
 
